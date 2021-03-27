6e119b97fadea9bb271507f0636c1dd5aa06d12e
Truls <Truls.stenrud@me.com>
2021-03-27 12:58:34 +0100

    Create python scripts for plotting results

---
+++ roborobo3/pyScripts/plotResults.py
@@ -0,0 +1,33 @@
+
+from os import listdir
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+def doStuff():
+    files = getResultFiles()
+    # plotFile(files[0])
+    for file in files:
+        plotFile(file)
+    plt.show()
+
+def getResultFiles():    
+    files = listdir()
+    files = list(filter(lambda x: x.endswith(".csv") and x.startswith("result"), files))
+    return files
+
+def plotFile(file):
+    nameSplit = file.rstrip(".csv").split("_")
+    name = nameSplit[1] + "_" + nameSplit[2]
+    
+    data = np.genfromtxt(file, delimiter=";")
+
+    plt.figure(name)    
+    plt.xlabel("Generations")
+    plt.ylabel("Fitness")
+    for test in data.T:
+        plt.plot(test)
+    
+
+if __name__ == "__main__":
+    doStuff()
---
+++ roborobo3/pyScripts/pyScript.py
@@ -0,0 +1,70 @@
+# This file is for gathering data from the fitness log files from different runs
+# Place the logfiles (NEAT_..., NoveltySearch_... etc.) in the same folder as this script and run this script from the same folder
+
+
+from os import listdir
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+def doStuff():
+    dic = groupFiles()
+
+    for key in dic.keys():
+        generateSummaryFile(key, dic[key])
+        
+def generateSummaryFile(name, fileNames):
+    files = [open(filename, 'r') for filename in fileNames]
+    csvReaders = [csv.reader(file, delimiter=';') for file in files]
+    
+    maxName = "result_" + name + "_max" ".csv"
+    meanName = "result_" + name + "_mean" + ".csv"
+    
+    with open(maxName, "w") as maxFile, open(meanName, "w") as meanFile:
+        maxWriter = csv.writer(maxFile, delimiter=";")
+        meanWriter = csv.writer(meanFile, delimiter=";")
+
+        # read all the files in paralell
+        for lines in zip(*csvReaders):
+            numbers = [[float(value) for value in line[1:]] for line in lines]
+            maxValues = [max(values) for values in numbers]    
+            meanValues = [sum(values)/len(values) for values in numbers]    
+            maxWriter.writerow(maxValues)
+            meanWriter.writerow(meanValues)
+
+    data = np.genfromtxt(maxName, delimiter=";")
+    print(data)
+
+    for test in data.T:
+        plt.plot(test)
+    
+    plt.show()
+    for file in files:
+        file.close
+
+
+
+def groupFiles():
+    dic = {}
+    
+    files = listdir()
+    files = filter(lambda x: x.endswith(".csv") and not x.startswith("result"), files)
+    
+    for fil in files:
+        key = fil.split("_")[0]
+        if key in dic.keys():
+            dic[key].append(fil)
+        else:
+            dic[key] = [fil]
+    return dic
+    
+
+if( __name__ == "__main__"):
+    doStuff()
+    
+    # items = [1, 3, 2]
+    # plt.plot(items)
+    # plt.show()
+    # items = [1, 2, 3]
+    # doubled = x * 2 for x in items
+    # map_dob = list(map(lambda x: x*2, items))
